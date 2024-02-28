2c18ecef3a2f34fa7cc5c940f8defb6413ea0462
ashwani <pydev.ashwani@gmail.com>
2024-02-28 11:23:31 +0530

    commit in idea

--- src/com/javamethod/Questions.java
+++ src/com/javamethod/Questions.java
@@ -5,10 +5,30 @@ import java.util.Scanner;
 public class Questions {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Pleas enter a number: ");
-        int n = in.nextInt();
-        System.out.println(isPrime(n));
+//        System.out.print("Pleas enter a number: ");
+//        int n = in.nextInt();
+//        System.out.println(isPrime(n));
+
+
+        for (int i = 100; i < 1000; i++) {
+            if (isArmstrong(i))
+                System.out.print(i + " ");
+        }
     }
+    //print all the 3 digits armstrong numbers;
+    static boolean isArmstrong(int n){
+        int original = n;
+        int sum = 0;
+
+        while (n > 0){
+            int rem = n % 10;
+            n = n / 10;
+            sum = sum + rem*rem*rem;
+        }
+
+        return sum == original;
+    }
+
 
     // wap method to check number prime or not?
     static boolean isPrime(int n){
